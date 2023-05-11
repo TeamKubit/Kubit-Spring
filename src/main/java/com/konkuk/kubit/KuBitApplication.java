bd7a864fe26ebb072bc153b4f9694a1497571b71
choieastsea <choieastsea@gmail.com>
2023-05-11 21:49:10 +0900

    [INIT] init spring boot

---
+++ src/main/java/com/konkuk/kubit/KuBitApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.kubit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuBitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuBitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/kubit/KuBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.kubit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
