9f5b95d0f44898008128746d94fcad107b09921e
choieastsea <choieastsea@gmail.com>
2023-05-28 16:12:33 +0900

    [FIX] AOP 다른 parameter와 사용할 수 있도록

--- src/main/java/com/konkuk/kubit/utils/UserIdAspect.java
+++ src/main/java/com/konkuk/kubit/utils/UserIdAspect.java
@@ -14,19 +14,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class UserIdAspect {
     @Autowired
     private UserRepository userRepository;
 
-    @Around("execution(* *(.., @GetUser (*), ..))") //@GetUser를 파라미터로 하는 것에서 User가져올 수 있도록 함
+    @Around("execution(* *(.., @GetUser (*), ..))") //@GetUser를 파라미터로 하는 것에서 User가져올 수 있도록 함 (출처 https://haviyj.tistory.com/36)
     public Object testAop(ProceedingJoinPoint joinPoint) throws Throwable {
         //return user after check user is authorized
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = userRepository.findByUserId(authentication.getName())
                 .orElseThrow(() -> new AppException(ErrorCode.USERID_NOTFOUND, "해당하는 사용자가 없습니다"));
-        Object result = joinPoint.proceed(new Object[]{user}); //user 정보 인자에 주입
-        return result;
+        Object[] args = Arrays.stream(joinPoint.getArgs()).map(data -> {
+            if (data instanceof User) {
+                data = user;
+            }
+            return data;
+        }).toArray();
+        return joinPoint.proceed(args);
     }
 }
