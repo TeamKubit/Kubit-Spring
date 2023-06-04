b4def2273627866373c5684e52a29eac311c4c4e
choieastsea <choieastsea@gmail.com>
2023-06-04 23:47:20 +0900

    [FIX] 거래 시간 분 까지만 나오도록

--- src/main/java/com/konkuk/kubit/domain/dto/BankDto.java
+++ src/main/java/com/konkuk/kubit/domain/dto/BankDto.java
@@ -1,6 +1,7 @@
 package com.konkuk.kubit.domain.dto;
 
 import com.konkuk.kubit.domain.Bank;
+import com.konkuk.kubit.utils.TransactionUtil;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,10 +10,10 @@ import java.time.LocalDateTime;
 public class BankDto {
     private String bankType; // 입금/출금
     private int money; // 입금/출금액
-    private LocalDateTime tradeAt;
+    private String tradeAt;
     public BankDto(Bank b){
         setBankType(b.getBankType());
         setMoney(b.getMoney());
-        setTradeAt(b.getTradeAt());
+        setTradeAt(TransactionUtil.getTimeString(b.getTradeAt()));
     }
 }
